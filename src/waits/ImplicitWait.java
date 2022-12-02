f10796d6ed7223a93ddf86d40a6a1f0929593687
velocity.ctc@gmail.com <velocity.ctc@gmail.com>
2022-12-02 10:36:59 +0530

    Waits in selenium

--- src/waits/ImplicitWait.java
+++ src/waits/ImplicitWait.java
@@ -1,17 +1,19 @@
 package waits;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class StaticWait2 {
+public class ImplicitWait {
 	
 	
 	
 	
-	
+
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 
@@ -20,16 +22,19 @@ public class StaticWait2 {
 		driver.get("https://www.google.com/");
 		
 		driver.findElement(By.xpath("//*[@name='q']")).sendKeys("India");
-		
-		
-		
+				
 		Actions act = new Actions(driver);
 		
 		act.sendKeys(Keys.ENTER).perform();
 		
-		Thread.sleep(60000);
+//		applying implicit wait:
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
 		driver.findElement(By.xpath("//*[text()='Central Bank of India']")).click();
 		
+		
+	
 		
 		
 		
