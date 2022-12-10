04c35f73c91981a466f1e54aa9390589c3226205
velocity.ctc@gmail.com <velocity.ctc@gmail.com>
2022-12-10 10:42:39 +0530

    testng

--- src/testngdiscussion/Annotations.java
+++ src/testngdiscussion/Annotations.java
@@ -1,10 +1,19 @@
 package testngdiscussion;
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Annotations {
+	
+	@BeforeTest
+	public void beforeTest()
+	{
+		
+	}
 	
 	@AfterMethod
 	public void afterMethod()
@@ -28,6 +37,19 @@ public class Annotations {
 	public void testCase2()
 	{
 		System.out.println("Test case 2");
+	}
+	
+	@BeforeClass
+	public void beforeClass()
+	{
+		System.out.println("Before class");
+	}
+	
+	
+	@AfterClass
+	public void afterClass()
+	{
+		System.out.println("After class");
 	}
 	
 	
